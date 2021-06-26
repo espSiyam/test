9da07c2b701870477c2305eff1911e7ea54d47bb
Siyam <42336409+espSiyam@users.noreply.github.com>
2021-06-26 21:42:33 +0600

    version1

---
+++ pointer.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string food = "Pizza";
+    string* ptr = &food;
+    cout <<food<<endl;
+    cout <<"Memory address: "<<&food<<endl;
+    cout <<"Address by pointer: " <<ptr;
+}
